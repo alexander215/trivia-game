d6e1adc81d384ac2885ae4d76983c5b6bab9e9fb
alexander215 <alexander.hoskinson@colorado.edu>
2019-07-08 15:06:16 -0700

    Basic question section

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log('test');
+
+const start = document.querySelector('button');
+start.addEventListener('click', () => {
+    console.log("Start button clicked.")
+})
+
+
+
+
+
+const answersContainer = document.querySelector('.answers-container');
+
+answersContainer.addEventListener('click', (e) => {
+    console.log(e.target.innerText);
+})
+
