0eec241882d7d54784df5438e880e2dff633979c
alexander215 <alexander.hoskinson@colorado.edu>
2019-07-08 16:46:15 -0700

    Display one question when start button is clicked

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 const start = document.querySelector('button');
 start.addEventListener('click', () => {
     console.log("Start button clicked.")
+    askQuestion();
 })
 
 
@@ -11,6 +12,20 @@ start.addEventListener('click', () => {
 const answersContainer = document.querySelector('.answers-container');
 
 answersContainer.addEventListener('click', (e) => {
-    console.log(e.target.innerText);
+    // console.log(e.target.innerText);
+
 })
 
+const askQuestion = () => {
+    const questionDisplay = document.querySelector('.question');
+    questionDisplay.innerText = question1.question;
+    // const answerDisplay = document.querySelector('#answer-1');
+    // answerDisplay.innerText = question1.answer[0];
+    const answerDisplay = document.querySelectorAll('.answers');
+    for (i = 0; i < 4; i++){
+        const answerDisplay = document.querySelectorAll(`.answers`);
+        answerDisplay[i].innerText = question1.answers[i]
+
+
+    }
+}
---
+++ js/questions.js
@@ -0,0 +1,17 @@
+// const questionsArray = [
+//     [
+// "What is the capital of California?", "Sacramento", "Los Angeles", "San Francisco", "San Diego"
+//     ]   
+// ]
+
+class Questions {
+    constructor(q,a,c){
+        this.question = q;
+        this.answers = a;
+        this.correctAnswer= c;
+    }
+}
+
+const question1 = new Questions("What is the capital of California?", ["Sacramento", "Los Angeles", "San Francisco", "San Diego"], 0);
+const question2 = new Questions("How many continents are there?", ["4", "1", "7", "12"], 2);
+const question3 = new Questions("Ancient Rome was centered around what modern-day country?", ["Romeland", "Italy", "Romania", "Germany"], 1);
