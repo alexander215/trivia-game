21cd7396a613ac6ce1ebcc2d0efd56c332f0233e
alexander215 <alexander.hoskinson@colorado.edu>
2019-07-09 10:56:24 -0700

    Randomly chooses question

--- js/app.js
+++ js/app.js
@@ -9,14 +9,24 @@ hideBoard.style.visibility = "hidden";
 // Click the start button to load the questions
 const start = document.querySelector('button');
 start.addEventListener('click', () => {
-    console.log("Start button clicked.")
+    // console.log("Start button clicked.")
     askQuestion();
 })
 
 // Launch a question when the start button is clicked
 const askQuestion = () => {
-    newQuestionSelector = question1;
+    newQuestionSelector = questions[randomQuestion()];
+    console.log(questions)
+    // newQuestionSelector = question1;
+    console.log(newQuestionSelector, "<--new question selector");
+    // console.log(`${newQuestionSelector}.correctAnswer`);
+    // console.log(newQuestionSelector.correctAnswer, "<--new question selector.correctAnswer");
+    // console.log(question1.correctAnswer, "<--question1.correctAnswer");
+    // console.log(parseInt(newQuestionSelector.correctAnswer), "<--parseInt(newQuestionSelector.correctAnswer)");
     newQuestionCorrectAnswer = newQuestionSelector.correctAnswer;
+    // newQuestionCorrectAnswer = newQuestionSelector.concat(`.${correctAnswer}`);
+
+
     hideBoard.style.visibility = "visible";
     const questionDisplay = document.querySelector('.question');
     questionDisplay.innerText = newQuestionSelector.question;
@@ -26,18 +36,32 @@ const askQuestion = () => {
     }
 }
 
+const randomQuestion = () => {
+    let random = Math.floor(Math.random() *3)
+    // let random = 'question';
+    // random = random.concat(random1);
+    // console.log(random1);
+    // console.log(random);
+    return random;
+
+}
+
 const answersContainer = document.querySelector('.answers-container');
 answersContainer.addEventListener('click', (e) => {
     // console.log(e.target.innerText);
     // console.log(e.target.id);
-    checkAnswer(e.target.id);
+    // checkAnswer(e.target.id);
+    checkAnswer(e.target.innerText);
+
+
 
 })
 
 checkAnswer = (num) => {
     // console.log((question1.correctAnswer), "<question1[correctAnswer");
     // console.log(num, "<--num");
-    if (parseInt(num) === (newQuestionCorrectAnswer)) {
+    // if (parseInt(num) === (newQuestionCorrectAnswer)) {
+    if(num === newQuestionCorrectAnswer){
         console.log("Correct!")
     } else {
         console.log("WRong!")
--- js/questions.js
+++ js/questions.js
@@ -6,6 +6,7 @@ class Questions {
     }
 }
 
-const question1 = new Questions("What is the capital of California?", ["Sacramento", "Los Angeles", "San Francisco", "San Diego"], 0);
-const question2 = new Questions("How many continents are there?", ["4", "1", "7", "12"], 2);
-const question3 = new Questions("Ancient Rome was centered around what modern-day country?", ["Romeland", "Italy", "Romania", "Germany"], 1);
+const question1 = new Questions("What is the capital of California?", ["Sacramento", "Los Angeles", "San Francisco", "San Diego"], "Sacramento");
+const question2 = new Questions("How many continents are there?", ["4", "1", "7", "12"], "7");
+const question3 = new Questions("Ancient Rome was centered around what modern-day country?", ["Romeland", "Italy", "Romania", "Germany"], "Italy");
+const questions = [question1, question2, question3]
