e5ed0a83f4217d738ab985a62f777ba37fdc5851
alexander215 <alexander.hoskinson@colorado.edu>
2019-07-09 17:14:02 -0700

    Created user and computer player class

---
+++ js/players.js
@@ -0,0 +1,13 @@
+class Player {
+    constructor(player1){
+        this.name = player1;
+        this.score = 0;
+    }
+}
+
+class Computer {
+    constructor(){
+    this.name = "Your Opponent";
+    this.score = 0;
+    }
+}
